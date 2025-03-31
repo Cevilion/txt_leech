4d2ad2311bc03acf922ac7344d577334eaee377d
Cevilion <sumitsajwan135@gmail.com>
2025-03-31 20:59:20 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "22403100"
+api_hash = "ccbc3f662735abfa604ef6309ba76e67"
+bot_token = "8056288503:AAGLPgmF8OA4mKtVsittxiKtq0GM-mwlzKQ"
